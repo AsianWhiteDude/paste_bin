1ee99a4e0b05f4b5fd4e5e2a8553ab3b698374ec
Mikhail Kim <kimisharik@gmail.com>
2024-07-30 21:49:07 +0300

    Changed content to a textfield

--- paste_bin/post_bin/models.py
+++ paste_bin/post_bin/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Paste(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, null=False)
-    content = models.CharField(max_length=255)
+    content = models.TextField()
     s3_link = models.URLField()
     hash_value = models.CharField(max_length=255, unique=True)
     time_create = models.DateTimeField(auto_now_add=True)
